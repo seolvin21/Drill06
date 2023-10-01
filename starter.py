ae03b927580a5e807e3036dcebb5fa4fb682e1ec
seolvin21 <129517269+seolvin21@users.noreply.github.com>
2023-10-01 15:40:20 +0900

    adjusted character direction

--- starter.py
+++ starter.py
@@ -12,11 +12,19 @@ def character_move():
     global arrive
     global hand_pos
     global x, y, i
+    global character_left, character_right
 
     if arrive:
         return
 
     if len(hand_pos) > 0:
+        if hand_pos[0][0] < x:
+            character_left = True
+            character_right = False
+        elif hand_pos[0][0] > x:
+            character_left = False
+            character_right = True
+
         t = i / 100 / 100
         x = (1 - t) * x + t * hand_pos[0][0]
         y = (1 - t) * y + t * hand_pos[0][1]
@@ -44,12 +52,12 @@ def handle_events():
 
 
 
-running = True
+running, arrive = True, False
+character_left, character_right = False, True
 hand_x, hand_y = 0, 0
 hand_pos = []
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 i = 0
-arrive = False
 frame = 0
 
 while running:
@@ -65,7 +73,11 @@ while running:
 
     for hand_x, hand_y in hand_pos:
         hand.clip_draw(0, 0, 50, 52, hand_x, hand_y)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+
+    if character_left:
+        character.clip_composite_draw(frame * 100, 100 * 1, 100, 100, 0, 'h', x, y, 100, 100)
+    elif character_right:
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
 
     frame = (frame + 1) % 8
 
